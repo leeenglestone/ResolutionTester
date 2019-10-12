eb0e6382fed18c4fd68c793003cfbe25a4e91536
Lee Englestone <lee.englestone@gmail.com>
2019-10-12 11:48:46 +0100

    Initial commit

---
+++ ResolutionTester.Solution/ResolutionTester.CoreConsoleApplication/Program.cs
@@ -0,0 +1,49 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+
+namespace ResolutionTester.CoreConsoleApplication
+{
+    class Program
+    {
+        const string url = "https://leasing.com";
+
+        static void Main(string[] args)
+        {
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("headless");
+            chromeOptions.AddArguments("hide-scrollbars");
+
+            using (var driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), chromeOptions))
+            {
+                var resolutions = Resolutions.GetResolutions();
+
+                foreach (var resolution in resolutions)
+                {
+                    Console.WriteLine($"Processing {resolution}");
+
+                    var dimensions = resolution.Split(new string[] { "x" }, StringSplitOptions.RemoveEmptyEntries);
+
+                    // Change window dimensions
+                    driver.Manage().Window.Size = new Size(int.Parse(dimensions[0]), int.Parse(dimensions[1]));
+
+                    // Navigate to page
+                    driver.Navigate().GoToUrl(url);
+
+                    // Generate screenshot of each page
+                    Screenshot image = ((ITakesScreenshot)driver).GetScreenshot();
+
+                    var fileName = $"{resolution}.png";
+
+                    //Save locally
+                    var path = Path.Combine(@"c:\tests\", fileName);
+                    image.SaveAsFile(path, ScreenshotImageFormat.Png);
+                }
+            }
+
+        }
+    }
+}
---
+++ ResolutionTester.Solution/ResolutionTester.CoreConsoleApplication/Resolutions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace ResolutionTester.CoreConsoleApplication
+{
+    public class Resolutions
+    {
+        public static IEnumerable<string> GetResolutions()
+        {
+            return new[]
+            {
+                "375x667",
+                "375x812",
+                "414x896",
+                "1920x1080",
+                "768x1024",
+                "414x736",
+                "360x640",
+                "360x740",
+                "412x846",
+                "1366x768",
+            };
+        }
+    }
+}
